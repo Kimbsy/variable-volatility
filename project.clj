29c378a701a0298792dca002193dd6cd2689c277
Kimbsy <lordkimber@gmail.com>
2021-10-14 23:14:53 +0100

    Bump quip version
    This should fix the audio crash bug

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject variable-volatility "0.1.0"
   :description "Game submissoin for Ludum Dare 49"
   :dependencies [[org.clojure/clojure "1.10.1"]
-                 [quip "1.0.13"]]
+                 [quip "1.0.15"]]
   :main ^:skip-aot variable-volatility.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
